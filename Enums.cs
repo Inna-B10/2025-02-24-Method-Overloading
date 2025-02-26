86c4b228cc37d477cf1126e897c844dd3163d1f9
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-02-26 02:25:36 +0100

    first commit

---
+++ Enums.cs
@@ -0,0 +1,13 @@
+namespace _2025_02_24_Method_Overloading;
+
+/* --------------------------------- Length --------------------------------- */
+public enum LengthUnits
+{
+  meters, kilometers, miles, feet
+}
+
+/* -------------------------------- Currency -------------------------------- */
+public enum CurrencyUnits
+{
+  USD, EUR, NOK, ILS, GBP
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using _2025_02_24_Method_Overloading.Converters;
+using _2025_02_24_Method_Overloading.Utils;
+
+namespace _2025_02_24_Method_Overloading;
+
+partial class Program
+{
+    /* ---------------------------------- Main ---------------------------------- */
+    static void Main()
+    {
+        Console.WriteLine("Hello, user!");
+        string choice = InputHandler.GetUserChoice();
+
+        if (choice == "error")
+        {
+            Console.WriteLine("Some error occurred. The program is terminating.");
+            return;
+        }
+        if (choice == "length") LengthConverter.Converter();
+
+        if (choice == "currency") CurrencyConverter.Converter();
+    }
+}
+
+
+
